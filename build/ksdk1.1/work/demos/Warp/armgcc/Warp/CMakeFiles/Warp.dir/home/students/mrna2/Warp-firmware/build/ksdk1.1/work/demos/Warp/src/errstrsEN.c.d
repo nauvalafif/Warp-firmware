29c577c372c25f0fcb7af2961b375a77e7ea62b0
Muhammad Rizqi Nauval Afif <nauval.afif@caditglobal.com>
2021-11-02 18:32:13 +0000

    delete build files

--- build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/home/students/mrna2/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/errstrsEN.c.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Warp.dir/home/students/mrna2/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/errstrsEN.c.obj: \
- /home/students/mrna2/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/errstrsEN.c
