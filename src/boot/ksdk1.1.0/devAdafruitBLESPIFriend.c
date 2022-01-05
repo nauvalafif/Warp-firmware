b445ca96429af193627749f447eff0dd735bb916
Muhammad Rizqi Nauval Afif <nauval.afif@caditglobal.com>
2022-01-05 15:52:45 +0000

    continuing BLE driver

--- src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.c
+++ src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.c
@@ -2,10 +2,16 @@
 // Created by Muhammad Rizqi Nauval Afif on 05/01/2022.
 //
 
+// Credit to Erich Styger https://github.com/ErichStyger/mcuoneclipse/tree/master/Examples/KDS/tinyK20/tinyK20_Adafruit_BLE
+
 /*
  *	config.h needs to come first
  */
 #include "config.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "fsl_spi_master_driver.h"
 #include "fsl_port_hal.h"
@@ -13,4 +19,29 @@
 #include "SEGGER_RTT.h"
 #include "gpio_pins.h"
 #include "warp.h"
-#include "devAdafruitBLESPIFriend.h.h"
+
+#include "sdep.h"
+#include "devAdafruitBLESPIFriend.h"
+
+/* IRQ will be high to indicate data from the module */
+/* CS is low active */
+
+#define BLUEFRUIT_MODE_COMMAND   1
+#define BLUEFRUIT_MODE_DATA      0
+
+#define BLE_MAX_RESPONSE_SIZE  48
+#define BLE_MAX_AT_CMD_SIZE    100
+
+static uint8_t  _mode;
+static uint8_t  m_tx_buffer[SDEP_MAX_PACKETSIZE];
+static uint8_t  m_tx_count;
+
+#define SPI_CS_ENABLE()   BLE_CS_ClrVal()
+#define SPI_CS_DISABLE()  BLE_CS_SetVal()
+
+#define SPI_IGNORED_BYTE          0xFEu /**< SPI default character. Character clocked out in case of an ignored transaction. */
+#define SPI_OVERREAD_BYTE         0xFFu /**< SPI over-read character. Character clocked out after an over-read of the transmit buffer. */
+#define SPI_DEFAULT_DELAY_US      50
+
+static uint8_t rxDummy; /* dummy byte if we do not need the result. Needed to read from SPI register. */
+
--- src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.h
+++ src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.h
@@ -2,8 +2,15 @@
 // Created by Muhammad Rizqi Nauval Afif on 05/01/2022.
 //
 
-#ifndef WARP_FIRMWARE_DEVADAFRUITBLESPIFRIEND_H
-#define WARP_FIRMWARE_DEVADAFRUITBLESPIFRIEND_H
+// Credit to Erich Styger https://github.com/ErichStyger/mcuoneclipse/tree/master/Examples/KDS/tinyK20/tinyK20_Adafruit_BLE
 
-#endif //WARP_FIRMWARE_DEVADAFRUITBLESPIFRIEND_H
+#include "sdep.h"
+#include "CLS1.h"
 
+uint8_t BLE_SendATCommandExpectedResponse(const uint8_t *cmd, uint8_t *rxBuf, size_t rxBufSize, const uint8_t *expectedTailStr);
+bool BLE_sendPacket(uint16_t command, const uint8_t *buf, uint8_t count, uint8_t more_data);
+bool BLE_getResponse(void);
+uint8_t BLE_Echo(bool on);
+bool BLE_IsConnected(void);
+uint8_t BLE_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io);
+void BLE_Init(void);
