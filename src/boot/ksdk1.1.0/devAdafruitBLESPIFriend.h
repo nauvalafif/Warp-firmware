b5c55d5255132e54dcd631e2891bfc5b628a12ed
Muhammad Rizqi Nauval Afif <nauval.afif@caditglobal.com>
2022-01-05 22:43:27 +0000

    delete CLS1.h mention

--- src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.h
+++ src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.h
@@ -5,7 +5,6 @@
 // Credit to Erich Styger https://github.com/ErichStyger/mcuoneclipse/tree/master/Examples/KDS/tinyK20/tinyK20_Adafruit_BLE
 
 #include "sdep.h"
-#include "CLS1.h"
 
 //uint8_t BLE_SendATCommandExpectedResponse(const uint8_t *cmd, uint8_t *rxBuf, size_t rxBufSize, const uint8_t *expectedTailStr);
 //bool BLE_sendPacket(uint16_t command, const uint8_t *buf, uint8_t count, uint8_t more_data);
