59f9c2ece0bf0173bf6206c8a8ddcca3f5a997dd
Muhammad Rizqi Nauval Afif <nauval.afif@caditglobal.com>
2022-01-05 12:19:09 +0000

    First attempt to create BLE driver

---
+++ src/boot/ksdk1.1.0/devAdafruitBLESPIFriend.c
@@ -0,0 +1,16 @@
+//
+// Created by Muhammad Rizqi Nauval Afif on 05/01/2022.
+//
+
+/*
+ *	config.h needs to come first
+ */
+#include "config.h"
+
+#include "fsl_spi_master_driver.h"
+#include "fsl_port_hal.h"
+
+#include "SEGGER_RTT.h"
+#include "gpio_pins.h"
+#include "warp.h"
+#include "devAdafruitBLESPIFriend.h.h"
