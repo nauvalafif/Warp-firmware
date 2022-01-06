2bb65f6391c398732c3a87dbff8c4d156028bf89
nauval.afif <mr.nauvalafif@gmail.com>
2022-01-06 18:31:54 +0000

    compile 18:31 6 Jan 2022

--- build/ksdk1.1/work/demos/Warp/src/boot.c
+++ build/ksdk1.1/work/demos/Warp/src/boot.c
@@ -50,6 +50,7 @@
 #include "fsl_device_registers.h"
 #include "fsl_i2c_master_driver.h"
 #include "fsl_spi_master_driver.h"
+#include "fsl_spi_hal.h"
 #include "fsl_rtc_driver.h"
 #include "fsl_clock_manager.h"
 #include "fsl_power_manager.h"
